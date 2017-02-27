35b0cde6badcb48ebf40b471efd23258aac9924b
worldforger <ajhey15@hotmail.com>
2017-02-26 20:50:04 -0600

    Renamed LogInControl to LogInForm and moved it to seperate file
    Did what said above

--- react-practice/src/App.js
+++ react-practice/src/App.js
@@ -1,113 +1,7 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-
-
-function LoginButton(props) {
-  return (
-    <button onClick={props.onClick}>
-      Login
-    </button>
-  );
-}
-
-function LogoutButton(props) {
-  return (
-    <button onClick={props.onClick}>
-      Logout
-    </button>
-  );
-}
-
-function RegisterButton(props){
-  return(
-  <button onClick={props.onClick}>
-    Register
-  </button>
-  )
-}
-
-function LogInForm(props){
-  return(
-    <div>
-      <input type="text" placeholder="Enter Username..."
-        onChange={props.handleUserChange}/><br />
-      <input type="text" placeholder="Enter Password..." onChange={props.handlePasswordChange} /> <br />
-      <LoginButton onClick={props.handleLoginClick} />
-      <RegisterButton onClick={props.handleRegisterClick} />
-      <br/>
-      <font color="red">{props.errorMessage}</font>
-    </div>
-  );
-}
-
-class LoginControl extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {isLoggedIn: false, loggedInUser: '', userfield: '', passwordfield: '', errorMessage: ''};
-    this.handleLoginClick = this.handleLoginClick.bind(this);
-    this.handleLogoutClick = this.handleLogoutClick.bind(this);
-    this.handleUserChange = this.handleUserChange.bind(this);
-    this.handlePasswordChange = this.handlePasswordChange.bind(this);
-    this.handleRegisterClick = this.handleRegisterClick.bind(this);
-  }
-
-  handleLoginClick() {
-  //EXAMPLE. We will probably want a different implementation to work with the database
-   for(var i = 0; i < this.props.users.length; i++){
-     if(this.props.users[i].username == this.state.userfield &&
-       this.props.users[i].password == this.state.passwordfield){
-       this.setState({loggedInUser: this.state.userfield});
-       this.setState({isLoggedIn: true});
-       this.setState({errorMessage: ''});
-       return;
-     }
-   }
-   this.setState({errorMessage: 'Incorrect Username or Password'})
-  }
-
-  handleRegisterClick(){
-    //Example. We will probably want a different implementation to work with the database
-    for(var i = 0; i < this.props.users.length; i++){
-      if(this.props.users[i].username == this.state.userfield){
-        this.setState({errorMessage: 'Username already taken'});
-        return;
-      }
-    }
-    addUser({username: this.state.userfield, password: this.state.passwordfield});
-    this.setState({loggedInUser: this.state.userfield});
-    this.setState({isLoggedIn: true});
-    this.setState({errorMessage: ''});
-  }
-
-  handleUserChange(e){
-    this.setState({userfield: e.target.value});
-  }
-
-  handlePasswordChange(e){
-    this.setState({passwordfield: e.target.value});
-  }
-
-  handleLogoutClick(){
-    this.setState({loggedInUser: '', isLoggedIn: false})
-  }
-
-  render() {
-    const isLoggedIn = this.state.isLoggedIn;
-    var display = null;
-    if (isLoggedIn) {
-      display = <LogoutButton onClick={this.handleLogoutClick}/>
-    } else {
-      display = <LogInForm handleUserChange={this.handleUserChange} handlePasswordChange={this.handlePasswordChange}
-        handleLoginClick={this.handleLoginClick} handleRegisterClick={this.handleRegisterClick} errorMessage={this.state.errorMessage}/>
-    }
-    return (
-      <div>
-          {display}
-      </div>
-    )
-  }
-}
+import LogInForm from './LogInForm'
 
 function addUser(user){
   users.push(user)
@@ -119,15 +13,37 @@ var users = [
 ];
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {isLoggedIn: false, loggedInUser: ''};
+    this.handleLogIn = this.handleLogIn.bind(this);
+    this.handleLogOut = this.handleLogOut.bind(this);
+  }
+
+  handleLogIn(user){
+    this.setState({isLoggedIn: true, loggedInUser: user});
+  }
+
+  handleLogOut(){
+    this.setState({isLoggedIn: false, loggedInUser: ''});
+  }
+
   render() {
+    var greeting = null;
+    if(this.state.isLoggedIn){
+      greeting = 'Welcome ' + this.state.loggedInUser;
+    } else{
+      greeting = 'Please Login...';
+    }
     return (
       <div className="App">
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h2>LogIn page</h2>
+          <h1>Scrumptious Demo Page</h1>
+          <h2>{greeting}</h2>
         </div>
         <br />
-        <LoginControl users={users} handleAddUser={this.addUser}/>
+        <LogInForm users={users} handleAddUser={this.addUser} handleLogIn={this.handleLogIn} handleLogOut={this.handleLogOut} isLoggedIn={this.state.isLoggedIn}/>
       </div>
     );
   }

6186d118fdb385be92d75bcf8f1e77da0054a4db
ndfroberg <104067@gmail.com>
2017-02-26 22:00:48 -0600

    slight changes to test
    still doesn't work, but baby-steps closer

--- react-practice/src/databaseClientTest.test.js
+++ react-practice/src/databaseClientTest.test.js
@@ -1,24 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import LogInForm from './LogInForm'
+import UserTable from './UserTable'
+import AddUserForm from './AddUserForm'
 
 beforeAll(() => {
-  addUser(mockName1, password, function(){});
+  //this is incorrectly called
+  addUser('mockName1', 'password1', function(){});
 });
 
 it('can find user', () => {
     //not sure what getUser is actually currently
     //returning. We could check username and
     //password independently?
-    expect(getUser(mockName, cb).toBeDefined());
+    expect(getUser('mockName', cb).toBeDefined());
 });
 
 it('can set logged in', () => {
-    this.setState({isLoggedIn: true, loggedInUser: user});
-    expect(getUser(mockName, cb).toBeTruthy());
+    setState({isLoggedIn: true, loggedInUser: user});
+    expect(getUser('mockName', cb).toBeTruthy());
 });
 
 it('can set logged out', () => {
-    this.setState({isLoggedIn: false, loggedInUser: user});
+    setState({isLoggedIn: false, loggedInUser: user});
     expect(this.state.isLoggedIn.toBeFalsy());
 });

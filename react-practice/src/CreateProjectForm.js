26b539c7178f35384b25dc6bac804bb2c1808103
Tyler Weiss <tnweiss5@gmail.com>
2017-03-19 22:25:32 -0500

    merging changes

--- react-practice/src/CreateProjectForm.js
+++ react-practice/src/CreateProjectForm.js
@@ -3,13 +3,22 @@ import React, { Component } from 'react';
 function CreateProjectDisplay(props){
   return(
     <div className="CreateProject">
-      Project Name*<br/>
+      Project Name* <font color="red">{props.errorMessage}</font><br/><br/>
       <input type="text" placeholder="Enter Project Name ... "
         onChange={props.handleProjectNameChange}/> <br/>
         Description<br/>
       <textarea align="bottom" cols="40" rows="2" type="text" placeholder="Enter Project Description ..." onChange={props.handleDescriptionChange} /> <br/>
-      <font color="red">{props.errorMessage}</font><br/>
-      <button onClick={props.handleCreateClick}>Create Project</button>
+      <button onClick={props.handleClick}>Create Project</button>
+    </div>
+  );
+}
+
+function AddUserDisplay(props){
+  return(
+    <div className="CreateProject">
+       Username*<br/>
+      <input type="text" placeholder="Enter Username ... "
+        onChange={props.handleProjectNameChange}/> <br/>
       <br/> <font color="red">{props.errorMessage}</font>
     </div>
   );
@@ -20,20 +29,22 @@ class CreateProjectForm extends Component {
 
   constructor(props) {
     super(props);
-    this.state = {projectTitle: '', descriptionField: '', errorMessage: ''};
-    this.handleCreateClick = this.handleCreateClick.bind(this);
+    this.state = {projectTitle: '', descriptionField: '', errorMessage: '', username:''};
     this.handleProjectNameChange = this.handleProjectNameChange.bind(this);
     this.handleDescriptionChange = this.handleDescriptionChange.bind(this);
+    this.handleUserNameChange = this.handleUserNameChange.bind(this);
+    this.handleClick = this.handleClick.bind(this);
   }
 
-  handleCreateClick(props){
+  handleClick(){
     if(this.state.projectTitle.length > 0){
         console.log("why wont this work!!!!!");
         //submit credentials to database here
         this.props.handleProjectComplete();
     }else{
       console.log("Should display error");
-      this.setState({errorMessage:'Please enter in a title for your project'});
+      this.setState({errorMessage:'Project name cannot be empty!'});
+      console.log(this.state.errorMessage + "<- should be something here!");
     }
   }
 
@@ -45,14 +56,18 @@ class CreateProjectForm extends Component {
     this.setState({descriptionField: e.target.value});
   }
 
+  handleUserNameChange(e){
+    this.setState({username: e.target.value});
+  }
+
   render() {
     return (
       <div className="CreateProject">
           <CreateProjectDisplay
             handleDescriptionChange={this.handleDescriptionChange}
             handleProjectNameChange={this.handleProjectNameChange}
-            handleCreateClick={this.handleCreateClick}
-            errorMessage={this.errorMessage}
+            handleClick={this.handleClick}
+            errorMessage={this.state.errorMessage}
           />
       </div>
     );

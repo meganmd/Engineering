a28e5fafa5874b70a9859094656157296d5d773d
worldforger <ajhey15@hotmail.com>
2017-03-20 16:39:34 -0500

    More test updates
    I updated the LogInForm test so that it can correctly test if it calls
    the getUser function that is passed into it. I think this is why the
    react apps I have seen have components take in methods rather than
    import.
    
    ALSO, to do this test, I installed enzyme and react-addons-test-utils.
    If you want it to work you should run npm install again. I also had to
    add a new class to the CSS file

--- react-practice/src/LogInForm.js
+++ react-practice/src/LogInForm.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 
 function LoginButton(props) {
   return (
-    <button onClick={props.onClick}>
+    <button className="logInButton" onClick={props.onClick}>
       Login
     </button>
   );
--- react-practice/src/LogInForm.test.js
+++ react-practice/src/LogInForm.test.js
@@ -2,31 +2,19 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import LogInForm from './LogInForm';
 import Client from './Client';
+import { mount } from 'enzyme';
 
 beforeAll(() => {
   Client.addUser('mockName1', 'password1', 'firstName1', 'lastName1', function(){});
 });
 
-// it('can register unique username', () => {
-//   // expect(LogInForm.state.registering).toBeFalsy;
-//   // LogInForm.handleRegisterClick();
-//   // expect(LogInForm.state.registering).toBeTruthy;
-//   done();
-// });
+test('LogInForm calls getUser when loginbutton is clicked', () => {
+  const getUser = jest.fn();
+  const wrapper = mount(
+    <LogInForm getUser={getUser} />
+  );
 
-
-// it('can register unique username', user => {
-//   function callback(user) {
-//     expect(user).toBeDefined;
-//     done();
-//   }
-//   // How to do this!?!
-//   LogInForm.handleUserChange('newUser1');
-//   LogInForm.handleSubmitNewUserClick();
-//   Client.getUser('newUser1',callback);
-// });
-
-
-it('knows that 2 and 2 make 4', () => {
-   expect(2 + 2).toBe(4);
- });
+  const p = wrapper.find('.logInButton');
+  p.simulate('click');
+  expect(getUser).toHaveBeenCalledTimes(1);
+});

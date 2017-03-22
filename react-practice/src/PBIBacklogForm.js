997927ce2d0a9ea39f86bd39c3ca78108814879c
Tyler Weiss <tnweiss5@gmail.com>
2017-03-22 00:33:47 -0500

    cleaned the PBIBacklog form and commented in whee the client calls will be made to query for user stories

--- react-practice/src/PBIBacklogForm.js
+++ react-practice/src/PBIBacklogForm.js
@@ -1,59 +1,82 @@
 import React, { Component } from 'react';
 
 function GetCardsForColumn(props) {
-  var idTags = ["a","b","c","d","e","f"];
-  var indents = [];
-  console.log(props.project.length + " this many projects");
-  for (var i = 0; i < props.project.length; i++) {
+  //var userStories = getUSerStories by column and project name
+  var userStories = props.project;
+  var content = [];
+  for (var i = 0; i < userStories.length; i++) {
     var divStyle = {
       position:"absolute",
-      background: "#bbbbbb",
+      background: "#dfdfdf",
       border: "2px solid black",
       left: "5%",
       top: "50px",
       width: "90%",
       'min-height': "100px",
-      'border-radius':"15px"
+      'border-radius':"15px",
     }
     divStyle.top = (i*125)+35;
-    console.log(props.backlogColumnStyle.height+ "Height--------" +divStyle.top);
     if(divStyle.top > parseInt(props.backlogColumnStyle.height,10)){
-      console.log("should be calling update");
       props.updateBoardHeight((i*125)+150);
     }
-    console.log(props.backlogColumnStyle.height+ "Height--------" +divStyle.top);
-    indents.push(<div /*className="card" id={idTags[i]}*/ style={divStyle}>{props.project[i].description}<br/>Size: {props.project[i].size}</div>);
+    content.push(<div style={divStyle}><br/>{userStories[i].description}<br/>Size: {userStories[i].size}</div>);
   }
   return(
-    <div id="productbacklog" style={props.backlogColumnStyle}>
-      <div id="title">Product Backlog</div>
-      {indents}
+    <div id={props.columnName} style={props.backlogColumnStyle}>
+      <div id="title">{props.title}</div>
+      {content}
     </div>
   );
-
 }
 
 function PBIBacklogDisplay(props){
-  console.log("got here");
+  //used only for testing layout as of now
+  var example = [{description:"This is a user story", size:"SMALL"},{description:"Make Food", size:"LARGE"},{description:"This is a user story", size:"SMALL"},{description:"Make Food", size:"LARGE"}];
+  var example1 = [{description:"Do some stuff", size:"SMALL"},{description:"Another user story", size:"LARGE"},{description:"Another user story", size:"LARGE"}];
+  var example2 = [{description:"Stop being lazy", size:"SMALL"},{description:"Another user story", size:"LARGE"}];
+  var example3 = [{description:"Finish the project", size:"SMALL"},{description:"Another user story", size:"LARGE"}];
+  var example4 = [{description:"This is another user story", size:"SMALL"}];
+
   return(
-    <div className="Backlog">
+    <div id="Backlog">
       <h1>{props.projectName}</h1>
-    <div id="board" style={{width:"100%", height:"fill"}}>
-      <GetCardsForColumn backlogColumnStyle={props.backlogColumnStyle}
-      updateBoardHeight={props.updateBoardHeight} project={props.project}/>
-      <div id="scrumbacklog" style={props.backlogColumnStyle}>
-        <div id="title">Scrum Backlog</div>
+    <div id="board">
+      <GetCardsForColumn
+        backlogColumnStyle={props.backlogColumnStyle}
+        updateBoardHeight={props.updateBoardHeight}
+        project={example}
+        columnName="productbacklog"
+        columnNumber="1"
+        title="Product Backlog"/>
+      <GetCardsForColumn
+        backlogColumnStyle={props.backlogColumnStyle}
+        updateBoardHeight={props.updateBoardHeight}
+        project={example1}
+        columnName="scrumbacklog"
+        columnNumber="2"
+        title="Scrum Backlog"/>
+      <GetCardsForColumn
+        backlogColumnStyle={props.backlogColumnStyle}
+        updateBoardHeight={props.updateBoardHeight}
+        project={example2}
+        columnName="todo"
+        columnNumber="3"
+        title="To Do"/>
+      <GetCardsForColumn
+        backlogColumnStyle={props.backlogColumnStyle}
+        updateBoardHeight={props.updateBoardHeight}
+        project={example3}
+        columnName="inprogress"
+        columnNumber="4"
+        title="In Progress"/>
+      <GetCardsForColumn
+        backlogColumnStyle={props.backlogColumnStyle}
+        updateBoardHeight={props.updateBoardHeight}
+        project={example4}
+        columnName="done"
+        columnNumber="5"
+        title="Done"/>
       </div>
-      <div id="todo" style={props.backlogColumnStyle}>
-        <div id="title">To Do</div>
-      </div>
-      <div id="inprogress" style={props.backlogColumnStyle}>
-        <div id="title">In Progress</div>
-      </div>
-      <div id="done" style={props.backlogColumnStyle}>
-        <div id="title">Done</div>
-      </div>
-    </div>
     </div>
   );
 }
@@ -74,11 +97,9 @@ class PBIBacklogForm extends Component {
       'border-radius': '10px'
     }
     this.updateBoardHeight = this.updateBoardHeight.bind(this);
-    this.example = [{description:"this is a user story", size:"small"},{description:"another user story", size:"large"}];
   }
 
   updateBoardHeight(e){
-    console.log("updating here " + e);
     this.backlogColumnStyle.height = e;
   }
 
@@ -87,9 +108,8 @@ class PBIBacklogForm extends Component {
       <div className="PBIBacklogDisplay">
           <PBIBacklogDisplay
             backlogColumnStyle={this.backlogColumnStyle}
-          projectName={this.props.projectName}
-        project={this.example}
-      updateBoardHeight={this.updateBoardHeight}/>
+            projectName={this.props.projectName}
+            updateBoardHeight={this.updateBoardHeight}/>
       </div>
     );
   }

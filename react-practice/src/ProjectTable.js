b499d9a699eefb95c2bc311475072187c001b6db
worldforger <ajhey15@hotmail.com>
2017-03-22 12:41:35 -0500

    added tablerowfunction

--- react-practice/src/ProjectTable.js
+++ react-practice/src/ProjectTable.js
@@ -2,6 +2,11 @@ import React, { Component } from 'react';
 import './App.css';
 import Client from './Client.js';
 
+function tableRow(props){
+  return(
+    <tr onClick={props.fetchProject}><td>{props.username}</td></tr>
+  )
+}
 
 class ProjectTable extends Component {
 
@@ -33,7 +38,7 @@ class ProjectTable extends Component {
 render() {
   var tableBody = [];
   for(var i = 0; i < this.state.users.length; i++){
-    tableBody.push(<tr onClick={this.fetchProject}><td>{this.state.users[i].username}</td></tr>);
+    tableBody.push(<tableRow username={this.state.users[i]} fetchProject={this.fetchProject});
   }
 
   return (

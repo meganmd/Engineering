b191ed68bb1c91afa5db6807916e479f8c749633
Tyler Weiss <tnweiss5@gmail.com>
2017-03-23 14:17:02 -0500

    columns highlihted if they are dropable

--- react-practice/src/PBIBacklogForm.js
+++ react-practice/src/PBIBacklogForm.js
@@ -15,12 +15,13 @@ function GetCardsForColumn(props) {
       width: "90%",
       'min-height': "100px",
       'border-radius':"15px",
+      columnColor: "#"
     }
     divStyle.top = (i*125)+35;
     if(divStyle.top > parseInt(props.backlogColumnStyle.height,10)){
       props.updateBoardHeight((i*125)+150);
     }
-    content.push(<div id={i} className={props.columnNumber}  style={divStyle} draggable="true" onDragStart={props.drag}><br/>{userStories[i].description}<br/>Size: {userStories[i].size}</div>);
+    content.push(<div id={i} className={props.columnNumber}  style={divStyle} draggable="true" onDragEnd={props.onDragExit} onDragStart={props.drag}><br/>{userStories[i].description}<br/>Size: {userStories[i].size}</div>);
   }
 
   return(
@@ -46,7 +47,8 @@ function PBIBacklogDisplay(props){
         title="Product Backlog"
       drag={props.drag}
     drop={props.drop}
-  allowDrop={props.allowDrop}/>
+  allowDrop={props.allowDrop}
+onDragExit={props.onDragExit}/>
       <GetCardsForColumn
         backlogColumnStyle={props.backlogColumnStyle}
         updateBoardHeight={props.updateBoardHeight}
@@ -56,7 +58,8 @@ function PBIBacklogDisplay(props){
         title="Scrum Backlog"
       drag={props.drag}
     drop={props.drop}
-  allowDrop={props.allowDrop}/>
+  allowDrop={props.allowDrop}
+onDragExit={props.onDragExit}/>
       <GetCardsForColumn
         backlogColumnStyle={props.backlogColumnStyle}
         updateBoardHeight={props.updateBoardHeight}
@@ -66,7 +69,8 @@ function PBIBacklogDisplay(props){
         title="To Do"
       drag={props.drag}
     drop={props.drop}
-  allowDrop={props.allowDrop}/>
+  allowDrop={props.allowDrop}
+onDragExit={props.onDragExit}/>
       <GetCardsForColumn
         backlogColumnStyle={props.backlogColumnStyle}
         updateBoardHeight={props.updateBoardHeight}
@@ -76,7 +80,8 @@ function PBIBacklogDisplay(props){
         title="In Progress"
       drag={props.drag}
     drop={props.drop}
-  allowDrop={props.allowDrop}/>
+  allowDrop={props.allowDrop}
+onDragExit={props.onDragExit}/>
       <GetCardsForColumn
         backlogColumnStyle={props.backlogColumnStyle}
         updateBoardHeight={props.updateBoardHeight}
@@ -86,7 +91,8 @@ function PBIBacklogDisplay(props){
         title="Done"
       drag={props.drag}
     drop={props.drop}
-  allowDrop={props.allowDrop}/>
+  allowDrop={props.allowDrop}
+onDragExit={props.onDragExit}/>
       </div>
     </div>
   );
@@ -102,7 +108,8 @@ class PBIBacklogForm extends Component {
       scrumbacklog:[{description:"This is a user story", size:"SMALL"},{description:"Make Food", size:"LARGE"},{description:"This is a user story", size:"SMALL"},{description:"Make Food", size:"LARGE"}],
       todo:[{description:"Do some stuff", size:"SMALL"},{description:"Another user story", size:"LARGE"},{description:"Another user story", size:"LARGE"}],
       inprogress:[{description:"Stop being lazy", size:"SMALL"},{description:"Another user story", size:"LARGE"}],
-      done:[{description:"Finish the project", size:"SMALL"},{description:"Another user story", size:"LARGE"}]
+      done:[{description:"Finish the project", size:"SMALL"},{description:"Another user story", size:"LARGE"}],
+      isDragging:false
     };
 
     this.backlogColumnStyle = {
@@ -113,12 +120,14 @@ class PBIBacklogForm extends Component {
       outline: '0',
       background: '#f2f2f2',
       'box-sizing': 'border-box',
-      'border-radius': '10px'
+      'border-radius': '10px',
+
     }
     this.updateBoardHeight = this.updateBoardHeight.bind(this);
     this.drag=this.drag.bind(this);
     this.drop=this.drop.bind(this);
     this.allowDrop=this.allowDrop.bind(this);
+    this.dragexit=this.dragexit.bind(this);
   }
 
   updateBoardHeight(e){
@@ -130,14 +139,21 @@ class PBIBacklogForm extends Component {
   }
 
   drag(ev) {
+    this.setState({isDragging:true});
       ev.dataTransfer.setData("text", ev.target.id);
       ev.dataTransfer.setData("column",ev.target.className);
       ev.dataTransfer.setData("row", ev.target.id);
       console.log("Row = " +ev.target.id + " column = " + ev.target.className);
   }
 
+  dragexit(ev){
+    console.log("done dragging");
+    this.setState({isDragging:false});
+  }
+
   drop(ev) {
     ev.preventDefault();
+    this.setState({isDragging:false});
     console.log(ev.clientX + " XPos " + ev.clientY + " YPos ");
     var column = 0;
     column = parseInt(ev.dataTransfer.getData("column"),10);
@@ -200,12 +216,41 @@ class PBIBacklogForm extends Component {
       back.push(item);
       this.setState({done:back});
     }
-    console.log("dropped");
+
+    var y = ev.screenY;
+    if(y>250){
+      console.log("drop in row 1");
+    }else if(y>500){
+      console.log("drop in row 2");
+    }else if(y>750){
+      console.log("drop in row 3");
+    }else if(y>1000){
+      console.log("drop in row 4");
+    }else if(y>1250){
+      console.log("drop in row 5");
+    }else{
+      console.log("drop in row 0");
+    }
+    console.log("dropped " + y);
 
 }
 
 
   render() {
+    var content;
+
+    var divStyle = {
+      position:"absolute",
+      background: "#00002222",
+      width: "19.4%",
+      left: ".1%",
+      height:this.backlogColumnStyle.height - 8,
+      'border-radius':"15px",
+      border: "4px solid blue"
+    }
+    if(this.state.isDragging == true){
+      content = <div style={divStyle} onDrop={this.drop} onDragOver={this.allowDrop}></div>
+    }
     return (
       <div className="PBIBacklogDisplay">
           <PBIBacklogDisplay
@@ -219,7 +264,9 @@ class PBIBacklogForm extends Component {
             column2={this.state.scrumbacklog}
             column3={this.state.todo}
             column4={this.state.inprogress}
-            column5={this.state.done}/>
+            column5={this.state.done}
+            onDragExit={this.dragexit}/>
+            {content}
       </div>
     );
   }

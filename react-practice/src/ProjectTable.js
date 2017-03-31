9f16594d71eba26f0d72a1ef98dd849b9590a4b5
ndfroberg <104067@gmail.com>
2017-03-31 11:49:54 -0500

    projectTable things

--- react-practice/src/ProjectTable.js
+++ react-practice/src/ProjectTable.js
@@ -8,31 +8,11 @@ class ProjectTable extends Component {
 
   constructor(props) {
     super(props);
-    this.state = {users:[], projects:[]};
-    this.getUsers = this.getUsers.bind(this);
-    this.getProjects = this.getProjects.bind(this);
+    this.state = { projects:[]};
     this.fetchProject = this.fetchProject.bind(this);
 
   }
 
-  getUsers() {
-//This needs to get projects, I'm testing it on users currently
-    Client.getUsers((users) => {
-      this.setState({
-        users: users,
-      });
-    });
-  }
-
-
-
-    getProjects(user) {
-      Client.getProjectsByUser((projects) => {
-        this.setState({
-          projects:projects,
-        });
-      });
-    }
 
 
   fetchProject (project) {
@@ -42,21 +22,32 @@ class ProjectTable extends Component {
 
   }
 
+getProjects(){
+
+Client.getProjectsByUser(this.props.user.username,(projects)=>{
+  this.setState({projects:projects})
+});
+
+}
+
    componentWillMount() {
-     this.getUsers();
+     this.getProjects();
    }
 
 render() {
   var tableBody = [];
 
-  this.state.projects = this.getProjects(this.props.user);
 
-        tableBody.push(<tr ><th><font color = "blue">Project</font></th><th><font color = "blue">Description</font></th></tr>);
-        for(var i = 0; i < this.state.projects.length; i++){
-          var clicker = this.fetchProject(this.state.projects[i]);
-          tableBody.push(<tr onClick={clicker}>
-          <td><font color = "black">{this.state.projects[i].projectTitle}</font></td>
-          <td><title>{'row'+i}</title><font color = "black">{this.state.projects[i].descriptionField}</font></td></tr>);}
+
+    tableBody.push(<tr ><th><font color = "blue">Project</font></th><th><font color = "blue">Description</font></th></tr>);
+    for(var i = 0; i < this.state.projects.length; i++){
+      var clicker = this.fetchProject(this.state.projects[i]);
+      tableBody.push(<tr onClick={clicker}>
+      <td><font color = "black">{this.state.projects[i].projectTitle}</font></td>
+      <td><title>{'row'+i}</title><font color = "black">{this.state.projects[i].descriptionField}</font></td></tr>);}
+
+
+
 
 /*
 
@@ -79,7 +70,7 @@ render() {
 
 
     </center>
-    )}
-}
+    )
+}}
 
 export default ProjectTable;
